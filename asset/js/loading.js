ce13972705551d1c686ffe44f1cb1d4e64808be0
Fujie Chen <imaple7@gmail.com>
2018-09-19 12:35:25 -0500

    Made the first page great again!

---
+++ asset/js/loading.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
+    anchors: ['name', 'aboutme'],
+    sectionsColor: ['#77A6F7', '#FFFFFF', '#D3E3FC', '#FFFFFF'],
+});
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'asset/particles.json', function() {
+
+});
